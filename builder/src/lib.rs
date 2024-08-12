9bb8f799fa4358081701577598b54be666ba0848
yoshonabee <jhann0120@gmail.com>
2024-08-12 10:38:49 +0800

    complete builder test 06

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,122 @@
 use proc_macro::TokenStream;
+use proc_macro2;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput, Type, TypePath};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let name = &input.ident;
+    let builder_name = format_ident!("{name}Builder");
+    let vis = &input.vis;
+
+    let mut field_funcs = Vec::new();
+    let mut fields_def = Vec::new();
+    let mut build_def = Vec::new();
+
+    if let syn::Data::Struct(data) = &input.data {
+        if let syn::Fields::Named(fields) = &data.fields {
+            for field in fields.named.iter() {
+                let field_name = field.ident.as_ref().expect("field should have Identity");
+                let field_ty = field.ty.clone();
+
+                if let Some(ty) = extract_type_from_option(&field_ty) {
+                    field_funcs.push(quote! {
+                        #vis fn #field_name(&mut self, input: #ty) -> &mut Self {
+                            self.#field_name = Some(input);
+                            self
+                        }
+                    });
+                } else {
+                    field_funcs.push(quote! {
+                        #vis fn #field_name(&mut self, input: #field_ty) -> &mut Self {
+                            self.#field_name = input;
+                            self
+                        }
+                    });
+                }
+
+                fields_def.push(quote! {
+                    #field_name: #field_ty,
+                });
+
+                build_def.push(quote! {
+                    #field_name: self.#field_name.clone(),
+                })
+            }
+        } else {
+            panic!("Not implemented for unnamed fields")
+        }
+    }
+
+    let field_funcs = proc_macro2::TokenStream::from_iter(field_funcs);
+    let fields_def = proc_macro2::TokenStream::from_iter(fields_def);
+    let build_def = proc_macro2::TokenStream::from_iter(build_def);
+
+    let expanded = quote! {
+        use std::error::Error;
+
+        #[derive(Default)]
+        #vis struct #builder_name {
+            #fields_def
+        }
+
+        impl #builder_name {
+            #field_funcs
+
+            fn build(&self) -> Result<#name, Box<dyn Error>> {
+                Ok(#name {
+                    #build_def
+                })
+            }
+        }
+
+        impl #name {
+            #vis fn builder() -> #builder_name {
+                #builder_name::default()
+            }
+        }
+    };
+
+    // Hand the output tokens back to the compiler
+    expanded.into()
+}
+
+fn extract_type_from_option(field_ty: &Type) -> Option<&Type> {
+    if let Type::Path(TypePath { path, .. }) = &field_ty {
+        let segments_str = &path
+            .segments
+            .iter()
+            .map(|segment| segment.ident.to_string())
+            .collect::<Vec<_>>()
+            .join(":");
+        // Concatenate `PathSegment` into a string, compare and take out the `PathSegment` where `Option` is located
+        let option_segment = ["Option", "std:option:Option", "core:option:Option"]
+            .iter()
+            .find(|s| segments_str == *s)
+            .and_then(|_| path.segments.last());
+        let inner_type = option_segment
+            // Take out the generic parameters of the `PathSegment` where `Option` is located
+            // If it is not generic, it is not possible to be `Option<T>`, return `None`
+            // But this situation may not occur
+            .and_then(|path_seg| match &path_seg.arguments {
+                syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                    args,
+                    ..
+                }) => args.first(),
+                _ => None,
+            })
+            // Take out the type information in the generic parameter
+            // If it is not a type, it is not possible to be `Option<T>`, return `None`
+            // But this situation may not occur
+            .and_then(|generic_arg| match generic_arg {
+                syn::GenericArgument::Type(ty) => Some(ty),
+                _ => None,
+            });
+
+        inner_type
+    } else {
+        None
+    }
 }
